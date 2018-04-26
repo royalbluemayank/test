3b9d133b1c193f67838e2eba0b36b37400ce51b7
Mayank Gupta <mayank165024@gmail.com>
2018-04-26 23:30:48 +0530

    RingRing

---
+++ RingRing/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RingRing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListViewItem list;
+            for (int i = 0; i < 15; i++)
+            {
+                list = new ListViewItem();
+                
+                ListView1.Items.Add(i);
+            }
+        }
+        private void _bubbleImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void TextBlock_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (BorderPanel.IsVisible)
+            {
+                BorderPanel.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                BorderPanel.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}

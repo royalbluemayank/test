047bc11dd0c8e25a849ef13795aedbe15905bb73
Mayank Gupta <mayank165024@gmail.com>
2018-06-12 10:31:05 +0530

    V - 1.1.1.2 ( fix Barcode and some bugs)

--- RingRing/StoreClass.cs
+++ RingRing/StoreClass.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 
 namespace RingRing
 {
@@ -10,11 +7,12 @@ namespace RingRing
     {
         private static int _Id = -1;
         private string _TvsId;
-        public Store(string TvsId, string StoreName)
+        public Store(string TvsId, string StoreName, string Currency = "Rs.")
         {
             _Id++;
             this._TvsId = TvsId;
             this.StoreName = StoreName;
+            Store.Currency = Currency + " ";
             Orders = new ObservableCollection<OrderHistory>();
         }
         public int Id { get { return _Id; } }
@@ -34,6 +32,10 @@ namespace RingRing
                 return value.Trim();
             }
         }
+        public static string Currency
+        {
+            get; private set;
+        }
         public string fullTvsId
         {
             get
@@ -48,44 +50,52 @@ namespace RingRing
         }
         public override string ToString()
         {
-            return this.TvsId + " , " + this.StoreName;
+            return this._TvsId + " , " + this.StoreName;
         }
     }
 
     public class OrderHistory
     {
-        private string Datetime;
+        //private string Datetime;
         public string OrderNumber { get; }
         public decimal OrderAmount { get; }
         public string DateTime 
         {
-            get
-            {
-                return Convert.ToDateTime(this.Datetime).ToString("hh:mm tt MMMM dd++ yyyy").Replace("++", "th"); //12:55 PM February 26th 2018
-            }
-            set
-            {
-                this.Datetime = value;
-            }
+            get; private set;
+            //get
+            //{
+            //    DateTime dt = Convert.ToDateTime(this.Datetime);
+            //    return string.Format("{0:hh:mm:ss tt MMMM dd}{1} {0:yyyy}", dt , ((dt.Day % 10 == 1 && dt.Day != 11) ? "st": (dt.Day % 10 == 2 && dt.Day != 12) ? "nd"
+            //                                                                                                            : (dt.Day % 10 == 3 && dt.Day != 13) ? "rd": "th"));
+            //    //return Convert.ToDateTime(this.Datetime).ToString("hh:mm tt MMMM dd++ yyyy").Replace("++", "th"); //12:55 PM February 26th 2018
+            //}
+            //private set
+            //{
+            //    this.Datetime = value;
+            //}
         }
-        public ObservableCollection<OrderProduct> products
+        public ObservableCollection<Product> products
         {
             get; set;
         }
-        public OrderHistory(string OrderNumber, decimal OrderAmount, String DateTime )
+        public OrderHistory(string OrderNumber, decimal OrderAmount, string OrderDateTime )
         {
             this.OrderNumber = OrderNumber;
             this.OrderAmount = OrderAmount;
-            this.DateTime = DateTime;
-            products = new ObservableCollection<OrderProduct>();
+            this.DateTime = OrderDateTime;
+            products = new ObservableCollection<Product>();
         }
-    }
-
-    public class OrderProduct
-    {
-        public string ProductBarcode { get; set; }
-        public string ProductName { get; set; }
-        public decimal ProductAmount { get; set; }
+        public class Product
+        {
+            public string Barcode { get; set; }
+            public string ProductName { get; set; }
+            public decimal Amount { get; set; }
+            public string Currency { get { return Store.Currency; } }
+            public string ToJson()
+            {
+                return "{\"ProductBarcode\":\"" + this.Barcode + "\",\"TimeStamp\":\"" + System.DateTime.Now.ToString("MM/dd/yyyy H:mm:ss:ffff zzz") + "\"}"; //yyyyMMddHHmmssffff
+            }
+        }
 
     }
 }
